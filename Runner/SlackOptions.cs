a1de2a5b748b7a835d4006a90d40cc2357021ab3
Florian Scheel <florian.scheel@zkrd.de>
2022-03-10 22:02:07 +0100

    Fix nullable warnings for configuration classes

--- Runner/SlackOptions.cs
+++ Runner/SlackOptions.cs
@@ -2,13 +2,13 @@
 {
    public class SlackOptions
    {
-      public string Token { get; set; }
-      public Proxy Proxy { get; set; }
+      public string? Token { get; set; }
+      public Proxy? Proxy { get; set; }
    }
 
    public class Proxy
    {
-      public string Host { get; set; }
+      public string? Host { get; set; }
       public int Port { get; set; }
    }
 }
