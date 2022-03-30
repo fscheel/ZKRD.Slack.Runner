c7bdf6fa512299408a8a0bca77c02b46bc64d4b3
Florian Scheel <florian.scheel@zkrd.de>
2022-03-30 22:40:53 +0200

    Return to Configure based configuration of endpoints
    Attribute based config is not yet supported if unit testing the endpoints.

--- Zkrd.Slack.WebApiDemo/Endpoints/HelloWorldPost.cs
+++ Zkrd.Slack.WebApiDemo/Endpoints/HelloWorldPost.cs
@@ -6,8 +6,6 @@ using Zkrd.Slack.WebApiDemo.Services;
 
 namespace Zkrd.Slack.WebApiDemo.Endpoints
 {
-   [HttpPost("/helloworld/api/")]
-   [AllowAnonymous]
    public class HelloWorldPost: Endpoint<HelloWorldRequest>
    {
       private readonly ISlackService _slackService;
@@ -17,6 +15,13 @@ namespace Zkrd.Slack.WebApiDemo.Endpoints
       {
          _logger = logger;
          _slackService = slackService;
+      }
+
+      public override void Configure()
+      {
+         Verbs(Http.POST);
+         Routes("/helloworld/api/");
+         AllowAnonymous();
       }
 
       public override async Task HandleAsync(HelloWorldRequest req, CancellationToken ct)
